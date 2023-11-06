763cc77961129b83855fbfccd749caa82317ca46
Mykyta Prus <mike.prus@ekreative.com>
2023-11-06 10:06:49 +0200

    feat: add sidebar component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Workspaces from "./Workspaces";
+
+const Sidebar = () => {
+  return (
+    <div className="col-span-1 border border-gray">
+      <Workspaces />
+    </div>
+  );
+};
+
+export default Sidebar;
